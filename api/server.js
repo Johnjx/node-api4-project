96fabcec356d11e5a6d7441bfacfb3f17cef4de2
John Ruddock <john.jxr@pm.me>
2022-06-09 17:52:41 -0400

    add cors

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userModel = require('./users/usersModel');
+const cors = require('cors')
 
 const server = express();
 server.use(express.json())
+server.use(cors())
 
 server.get('/', (req, res) => {
     res.send(`<h1>${process.env.MESSAGE || 'Hello World!!'}</h1>`)
